eabc7ebca1b5edcf72aed9278918ea978cd53d5e
naenaetheman22-source <naenaetheman22@gmail.com>
2026-05-20 01:09:50 -0500

    Update site name and subtext in main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "Bens Unblocked Games"; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Luh Nate's Unblocked games"; // Change this to change the name of your website.
+var subtext = "v1"; // set the subtext
 
 // more settings in main.css
 
